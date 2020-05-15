7ece4e925d575125a7502640428c9a96a52226ba
abrantes <abrantes.nsc@gmail.com>
2020-05-15 20:05:54 -0300

    correcao com o elano Index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="GERADORZÃO" content="Jekyll v4.0.1">
     <title>Cover Template · Bootstrap</title>
   </cabeça>
-  
+
   <code>
     import os
     print('QUALQUER COISA')
@@ -28,14 +28,15 @@
 
       <main role="main" class="inner cover">
         <h1 class="cover-heading">Cover your page.</h1>
-        <p class="lead">Aqui vai ser uma paradas muito doida mesmo meu amigo. simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+        <p class="lead">Aqui vai ser uma paradas muito pitorescas mesmo. simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
         <p class="lead">
+          <h1>Só pra adicionar qualquer coisa</h1>
           <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
         </p>
       </main>
 
-      <footer class="mastfoot mt-autoMOVEL">
-        <div class="inner-LIGHT">
+      <footer class="mastfoot mt-autoBATMOVEL">
+        <div class="inner-DARK">
           <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
         </div>
       </footer>
